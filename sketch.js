9ff5a8d6e0c9a14829cb9d02f970841ae0d3c999
prachiArora05 <prachiarora981@gmail.com>
2021-07-10 18:41:05 +0530

    changed the bird's position

--- sketch.js
+++ sketch.js
@@ -15,7 +15,6 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -34,7 +33,7 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200, 200);
 
 }
 
